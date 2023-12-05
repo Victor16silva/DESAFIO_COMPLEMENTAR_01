e3ff8f7e874821d7f92cf61cc57ed8576a9595ab
Victor  Silva <43828449+vitor124vi@users.noreply.github.com>
2023-12-05 20:36:37 -0300

    Subindo o trabalho Complementar 01

---
+++ script.js
@@ -0,0 +1,17 @@
+//Exercício 01:
+var nomeInserido = prompt("Insira Seu nome:");
+var idade = prompt("Insira sua idade:")
+
+//Exercício 02:
+alert("Iremos começar a Multiplicação");
+
+
+var numero1 = prompt('Digite o numero 1');
+var numero2 = prompt('Digite o numero 2');
+
+var soma = parseInt(numero1) + parseInt(numero2);
+var multiplicacao = parseInt(numero1) * parseInt(numero2);
+
+// Exercício 03: 
+
+alert("Resultados da escolha é:" + nomeInserido +' '+ "Idade:" +"  "+ idade +' Multiplicação:'+"  "+ multiplicacao );
